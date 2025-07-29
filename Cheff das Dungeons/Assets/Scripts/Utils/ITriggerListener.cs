17faf6e218d5722c9585b9cce43fae632c72ed37
Juan Pimentel <juandbpimentel@gmail.com>
2025-07-29 19:02:24 +0000

    Criação de inimigo slime completa e outros inimigos inicial

---
+++ Cheff das Dungeons/Assets/Scripts/Utils/ITriggerListener.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+/// <summary>
+/// Define um "contrato" para qualquer componente que queira "ouvir"
+/// os eventos de um gatilho filho (como o Custom2DTrigger).
+/// </summary>
+public interface ITriggerListener
+{
+    void OnChildTriggerEnter2D(GameObject triggerObject, Collider2D other);
+
+    void OnChildTriggerExit2D(GameObject triggerObject, Collider2D other);
+
+    void OnChildTriggerStay2D(GameObject triggerObject, Collider2D other);
+}
+
